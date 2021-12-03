2127413ffd6ff266e7b6385794418d65e39f225c
gamburgm <mitch.gamburg@gmail.com>
2021-12-03 14:43:33 -0500

    basic step

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <cstdio>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 void runFile(char *);
 void runPrompt();
+void run(istream &);
 
 int main(int argc, char *argv[]) {
   if (argc > 2) {
@@ -14,10 +16,18 @@ int main(int argc, char *argv[]) {
   } else {
     runPrompt();
   }
+
+  return 0;
 }
 
-void runFile(char *filename) {
+void runFile(char *filepath) {
+  ifstream f (filepath, ios::in);
+  run(f);
+  f.close();
 }
 
 void runPrompt() {
+}
+
+void run(istream &src) {
 }
