4b7513bb191e88f73e2b8139f2e00d7dc186a085
gamburgm <mitch.gamburg@gmail.com>
2021-12-06 15:38:58 -0500

    some error handling and sketching

--- main.cpp
+++ main.cpp
@@ -6,9 +6,14 @@
 
 using namespace std;
 
+// TODO the runner family of functions should be modified to return error codes, or some processable error info.
+//      the book uses a global variable (member of the main class) which I won't do for various reasons.
+//      
 void runFile(char *);
 void runPrompt();
 void run(istream &);
+void error(int, string);
+void report(int, string, string);
 
 int main(int argc, char *argv[]) {
   if (argc > 2) {
@@ -26,6 +31,7 @@ void runFile(char *filepath) {
   ifstream f (filepath, ios::in);
   run(f);
   f.close();
+  // TODO if run returned an error, exit with error code 65.
 }
 
 void runPrompt() {
@@ -40,8 +46,23 @@ void runPrompt() {
 
     auto lstream = stringstream(line);
     run(lstream);
+    // TODO if run returned an error, ignore it. You'd reset the 'global variable' here.
   }
 }
 
+// TODO this function should be modified to return error (codes?), or some processable error info.
+//      the book uses a global variable (member of the main class) which I won't do for various reasons.
 void run(istream &src) {
+  // scanner = Scanner(src);
+  // vector toks = scanner.getTokens();
+  // for token in tokens:
+  //     cout << token << endl;
+}
+
+void error(int line, string message) {
+  report(line, string(""), message);
+}
+
+void report(int line, string where, string message) {
+  cout << "[ line " << line << "] Error" << where << ": " << message << endl;
 }
